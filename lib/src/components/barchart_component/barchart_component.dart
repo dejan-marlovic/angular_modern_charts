408d143b9149edc89ea3c6c4db0fe7df08c6bd57
afpatmin <patrick.minogue@appfoundry.se>
2017-12-24 22:47:25 +0100

    barchart mini update

--- lib/src/components/barchart_component/barchart_component.dart
+++ lib/src/components/barchart_component/barchart_component.dart
@@ -11,7 +11,6 @@ class BarChartComponent implements AfterViewInit, OnChanges
 {
   void ngAfterViewInit()
   {
-    chartRef.nativeElement.style.height = chartProperties.height;
     _chart = new BarChart(chartRef.nativeElement);
   }
 
